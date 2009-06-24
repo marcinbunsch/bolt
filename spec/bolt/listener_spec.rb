517cc068462ce09264f85363d9c3de879f0be419
marcinbunsch <marcin@applicake.com>
2009-06-24 22:34:10 +0200

    specs

--- spec/bolt/listener_spec.rb
+++ spec/bolt/listener_spec.rb
@@ -11,10 +11,6 @@ describe Bolt::Listener do
     instance.os.should == 'unknown system'
     instance.selected = nil
     instance.selected.should == nil
-    instance.stub(:os).and_return('i686darwin-1.10.1')
-    instance.os.should == 'i686darwin-1.10.1'
-    Bolt::Listeners::Osx.stub(:start).and_return('osx')
-    instance.listener.should == 'osx'
   end
   
   it 'should select appropriate listener' do
